c003008c658abf02267d59c4663f57de0b04b8fd
DipanshuRawat <110223176+DipanshuRawat@users.noreply.github.com>
2025-04-10 16:16:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ app.get('/', (req, res) => {
   res.send('Hello from Node.js app running on port 2000 🚀');
 });
 
+// Bind to 0.0.0.0 so Docker can expose it
 if (require.main === module) {
-  app.listen(PORT, () => {
-    console.log(`App running at http://localhost:${PORT}`);
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`App running at http://0.0.0.0:${PORT}`);
   });
 }
 
